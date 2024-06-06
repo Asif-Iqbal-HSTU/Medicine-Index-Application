ba2be6c8080f3af580c3ecec6ec536309d8fa423
AsifIqbal <asif.iqbal.hstu@gmail.com>
2024-06-06 21:31:02 +0600

    v for vendata

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
-echo " BUILD START"
+echo "BUILD START"
+
+# Install pip if not available
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END"
+
+echo "BUILD END"
--- medicine_index/settings.py
+++ medicine_index/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -134,4 +135,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
