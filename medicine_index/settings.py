03a92ac59ff929adf18df258237f3006b811091e
AsifIqbal <asif.iqbal.hstu@gmail.com>
2024-06-06 21:52:21 +0600

    railway

--- medicine_index/settings.py
+++ medicine_index/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
